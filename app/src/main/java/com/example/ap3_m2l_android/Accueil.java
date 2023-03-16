7adbe115c66c00d712629bca8423b174eb80bd54
Sourbe <99745843+Sourbe@users.noreply.github.com>
2023-03-16 09:26:31 +0100

    version

--- app/src/main/java/com/example/ap3_m2l_android/Accueil.java
+++ app/src/main/java/com/example/ap3_m2l_android/Accueil.java
@@ -3,6 +3,8 @@ package com.example.ap3_m2l_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Accueil extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class Accueil extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
+
+        Spinner spinner = (Spinner) findViewById(R.id.s_domaine);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.domaine_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }
