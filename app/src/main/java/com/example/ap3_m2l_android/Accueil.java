9403a42290886ce472503cc7aeca40a8065060d2
Sourbe <99745843+Sourbe@users.noreply.github.com>
2023-03-16 11:50:49 +0100

    Update Accueil.java

--- app/src/main/java/com/example/ap3_m2l_android/Accueil.java
+++ app/src/main/java/com/example/ap3_m2l_android/Accueil.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Accueil extends AppCompatActivity {
@@ -27,17 +28,25 @@ public class Accueil extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         final LinearLayout lm = (LinearLayout) findViewById(R.id.linearMain);
 
+        
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
 
+        List<String> list_domaines = new ArrayList<String>();
+
+        String[] domaines = myDbHelper.getDomaines();
+
+        list_domaines.add("Choisir un domaine");
+        Collections.addAll(list_domaines, domaines);
+
+
         Spinner domaine = (Spinner) findViewById(R.id.s_domaine);
         // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.domaine_array, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        domaine.setAdapter(adapter);
+        ArrayAdapter spinnerArrayAdapter = new ArrayAdapter(this,
+                android.R.layout.simple_spinner_dropdown_item,
+                list_domaines);
+        domaine.setAdapter(spinnerArrayAdapter);
 
         domaine.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
@@ -51,10 +60,7 @@ public class Accueil extends AppCompatActivity {
     }
     private void ShowFormationWithDomaineId(int ID){
         final LinearLayout lm = (LinearLayout) findViewById(R.id.linearMain);
-
-        for (int i = 0; i < ListButtons.size(); i++) {
-            lm.removeView(ListButtons.get(i));
-        }
+        lm.removeAllViews();
 
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
@@ -75,7 +81,7 @@ public class Accueil extends AppCompatActivity {
             // set the layoutParams on the button
             btn.setLayoutParams(params);
 
-            final int index = j;
+            final int index = j + 1;
             // Set click listener for button
             btn.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
